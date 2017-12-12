7b7b158000c839652ac258a3dd4e4e61bb014dee
James Ko <jamesqko@gmail.com>
2017-12-12 11:05:13 -0500

    Work

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,5 +1,11 @@
 #include "pipe_networking.h"
 
+#define TO_SERVER_PIPE "mario"
+#define TO_CLIENT_PIPE "luigi"
+
+#define TO_SERVER_MESSAGE_1 "Client message 1"
+#define TO_CLIENT_MESSAGE   "Server message"
+#define TO_SERVER_MESSAGE_2 "Client message 2"
 
 /*=========================
   server_handshake
@@ -11,6 +17,18 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_handshake(int *to_client) {
+  mkfifo(TO_SERVER_PIPE, 0644);
+  mkfifo(TO_CLIENT_PIPE, 0644);
+  int fd_read = open(TO_SERVER_PIPE, O_RDONLY);
+  int fd_write = open(TO_CLIENT_PIPE, O_WRONLY);
+  
+  char buf[256];
+  read(fd_read, buf, sizeof(buf));
+  if (strncmp(buf, TO_SERVER_MESSAGE_1, sizeof(TO_SERVER_MESSAGE_1))) {
+    printf("wrong message from client\n");
+    exit(1);
+  }
+  
   return 0;
 }
 
@@ -25,5 +43,21 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-  return 0;
+  int write_fd = open(TO_SERVER_PIPE, O_WRONLY);
+  int read_fd = open(TO_CLIENT_PIPE, O_RDONLY);
+
+  write(write_fd, TO_SERVER_MESSAGE_1, sizeof(TO_SERVER_MESSAGE_1));
+
+  char read_buf[256];
+  read(read_fd, read_buf, sizeof(read_buf));
+
+  if (strncmp(read_buf, TO_CLIENT_MESSAGE, sizeof(TO_CLIENT_MESSAGE)) != 0) {
+    fprintf(stderr, "Did not receive correct server message.");
+    exit(1);
+  }
+
+  write(write_fd, TO_SERVER_MESSAGE_2, sizeof(TO_SERVER_MESSAGE_2));
+
+  *to_server = write_fd;
+  return read_fd;
 }
